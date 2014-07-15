af00b4175b73b26d5cd9e5364a852cf3ce621535
MACHIDA Hideki <h@matchy.net>
2014-07-16 01:09:55 +0900

    背景の色を変えてみた

---
+++ assets/js/background.js
@@ -0,0 +1,24 @@
+$(function() {
+  var parent = prev = null;
+  var flg = 'odd';
+  $('#maincontent').children().each(function() {
+    if ($(this).prop('tagName') == 'H2') {
+      parent = null;
+    }
+    if (parent == null) {
+      var section;
+      if (flg == 'odd') {
+        section  = $('<div class="section odd">');
+        flg = 'even';
+      } else {
+        section  = $('<div class="section even">');
+        flg = 'odd';
+      }
+      $('#maincontent').append(section);
+      parent = $('<div class="container">');
+      section.append(parent);
+    }
+    $(this).remove();
+    parent.append($(this));
+  })
+})
