2e68a2d8be01f0e6d9e5bd6205c2d00a7fd656a4
MACHIDA 'matchy' Hideki <h@matchy.net>
2014-07-25 17:19:49 +0900

    外部リンクを集計できるように

--- assets/js/externallink.js
+++ assets/js/externallink.js
@@ -1,3 +1,14 @@
-$(document.links).filter(function() {
-    return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
+$(function() {
+  $(document.links).filter(function() {
+      return this.hostname != window.location.hostname;
+  }).attr('target', '_blank');
+});
+
+$(function() {
+  $(document.links).filter(function() {
+      return this.hostname != window.location.hostname;
+  }).click(function() {
+    trackOutboundLink(this.href);
+    return false;
+  });
+});
