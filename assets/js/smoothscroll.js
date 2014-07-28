b9440ba9ab0959bf276602e3b523c7150256354f
MACHIDA 'matchy' Hideki <h@matchy.net>
2014-07-28 15:36:01 +0900

    scroll to top ボタン追加

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,4 +1,23 @@
 $(function() {
+  var btnToTop = $('#scroll-to-top');
+  btnToTop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      btnToTop.fadeIn();
+    }else{
+      btnToTop.fadeOut();
+    }
+  });
+  btnToTop.click(function () {
+    $('body, html').animate({scrollTop:0}, 500, 'swing');
+    if (history.pushState) {
+      	history.pushState(null, null, '#');
+    }
+   return false;
+  });
+});
+
+$(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
