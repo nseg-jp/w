5e78e25ac521b80e128627408b16280aa08f25bb
MACHIDA Hideki <h@matchy.net>
2015-05-15 13:31:52 +0900

    画像の順序をシャッフル

--- assets/js/flickr-slideshow.js
+++ assets/js/flickr-slideshow.js
@@ -1,8 +1,10 @@
 $(function() {
+  vegas_imgs.sort(function() { return Math.random() - 0.5; });
+
   $.vegas('slideshow', {
     delay: 5000,
     backgrounds: vegas_imgs,
   })('overlay', {
     src:'assets/img/overlay.png'
-  });    
+  });
 });
